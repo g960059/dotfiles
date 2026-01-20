f280f43cc94c09c927035e6cdbaef3c6dc145849
VirtualMachine <virtualmachine@Apple-Virtual-Machine-1.local>
2026-01-20 08:10:23 -0800

    edit keifu.nix

--- dot_config/home-manager/pkgs/keifu.nix
+++ dot_config/home-manager/pkgs/keifu.nix
@@ -3,21 +3,21 @@
 , rustPlatform
 , fetchFromGitHub
 , pkg-config
-, libgit2
 , openssl
-, zlib
+, libgit2
+, git
 , darwin
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "keifu";
-  version = "0.2.2"; # GitHub Releases の Latest が v0.2.2 (2026-01-15) :contentReference[oaicite:3]{index=3}
+  version = "0.2.2";
 
   src = fetchFromGitHub {
     owner = "trasta298";
-    repo  = "keifu";
-    rev   = "v${version}";
-    hash  = lib.fakeSha256; # ← まずはこれでOK（後で置き換える）
+    repo = "keifu";
+    rev = "v${version}";
+    hash = "sha256-jMUhg3irMzTPV0TKaZU6/jiKCMAQLF0SJTUJdub4IA4=";
   };
 
   cargoLock = {
@@ -26,25 +26,20 @@ rustPlatform.buildRustPackage rec {
 
   nativeBuildInputs = [ pkg-config ];
 
-  # git2/libgit2 系を踏むことが多いので、darwin も含めて無難に入れておく
   buildInputs =
-    [ libgit2 openssl zlib ]
-    ++ lib.optionals stdenv.isDarwin [
-      darwin.libiconv
-      darwin.apple_sdk.frameworks.Security
-      darwin.apple_sdk.frameworks.SystemConfiguration
-      darwin.apple_sdk.frameworks.CoreFoundation
-    ];
+    [ openssl libgit2 git ]
+    ++ lib.optionals stdenv.isDarwin [ darwin.libiconv ];
 
-  # libgit2 を system からリンクさせる（vendored を避けやすい）
-  env.LIBGIT2_SYS_USE_PKG_CONFIG = "1";
+  # 依存が vendoring しないように寄せる（必要なら）
+  OPENSSL_NO_VENDOR = 1;
+  LIBGIT2_SYS_USE_PKG_CONFIG = 1;
 
   meta = with lib; {
-    description = "TUI tool to visualize Git commit graphs";
+    description = "Git genealogy, untangled. A TUI for navigating commit graphs";
     homepage = "https://github.com/trasta298/keifu";
     license = licenses.mit;
     mainProgram = "keifu";
-    platforms = platforms.darwin;
+    platforms = platforms.unix;
   };
 }
 
